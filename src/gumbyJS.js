a6f0c7e448b8a7212480b4f64b64c32454110b27
Eldon Kroll <ejkroll@gmail.com>
2022-09-14 15:52:42 -0500

    updated xiny and some tests

--- src/gumbyJS.js
+++ src/gumbyJS.js
@@ -9,32 +9,24 @@
  */
 
 gumby = (() => { 
-    let _this = {
-        fns: {
-            xiny: (x, y, i) => { 
-                let ns = (x || "").replace(/\[|\]/g, '.').split("."),
-                    p = function (n, o, i) {
-                        if(n == '' || n == '*') return o;
-                        if(!i) return o[n];
-                        for (let f in o) { if (f.toLowerCase() === n.toLowerCase()){ return o[f]; } } 
-                    };
-                for (let n in ns) { y = y ? p(ns[n], y, i) : y; } 
-                return y; 
-            }
-        }
-    };
-    return {
-        xiny: (x, y, i) => {
-            return _this.fns.xiny(x, y, i);
-        }
-    };
-})(window);
+    'use strict';
 
-gumby.object = (() => {
     let _this = {
         fns: {
+            extend: function () {
+                let s = _this.fns, e = {}, d = false, i = 0;
+                if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]') { d = arguments[0]; i++; }
+                let m = function (o) {
+                    for (var p in o) {
+                        if (!Object.prototype.hasOwnProperty.call(o, p)) continue;
+                        e[p] = d && Object.prototype.toString.call(o[p]) === '[object Object]' ? s.extend.apply(s, [true, e[p], o[p]]) : o[p];
+                    }
+                };
+                for (; i < arguments.length; i++)  m(arguments[i]);
+                return e;
+            },
             clone: (o) => {
-                if (o === null || typeof(o) !== 'object') return o;
+                if (o === null || typeof (o) !== 'object') return o;
                 let temp = o.constructor();
                 for (let p in o) {
                     if (Object.prototype.hasOwnProperty.call(o, p)) {
@@ -44,23 +36,25 @@ gumby.object = (() => {
                     }
                 }
                 return temp;
-            },
-            extend: function() {
-                let s = _this.fns, e = {}, d = false, i = 0;
-                if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) { d = arguments[0]; i++; }
-                let m = function (o) {
-                    for ( var p in o ) {
-                        if (!Object.prototype.hasOwnProperty.call( o, p ) ) continue;
-                        e[p] = d && Object.prototype.toString.call(o[p]) === '[object Object]' ? s.extend.apply(s, [true, e[p], o[p]]) : o[p];						
-                    }
-                };
-                for ( ; i < arguments.length; i++ )  m(arguments[i]);
-                return e;
+            },            
+            xiny: (x, y, i) => {
+                let ns = (x || "").replace(/\[|\]/g, '.').split(".").filter(n => { return n != ""; }),
+                    fn = (ns, o, c) => {
+                        let n = ns[0];
+                        if (!n || !o) return o;
+                        ns = ns.slice(1);
+                        if (Array.isArray(o) && n === "*") return o.map(e => fn(ns, e, c));
+                        if (!c) return fn(ns, o[n] !== undefined ? o[n] : undefined, c);
+                        for (let f in o)
+                            if (f.toLowerCase() === n.toLowerCase())
+                                return fn(ns, o[f], c);
+                    };                
+                return y ? fn(ns, y, i) : y;
             },
             xtoy: (x, y, e) => {
                 x = x || {};
                 y = y || {};
-                for(let p in x)
+                for (let p in x)
                     y[p] = e ? y[p] ? x[p] : y[p] : x[p];
                 return y;
             }
@@ -68,19 +62,150 @@ gumby.object = (() => {
     };
 
     return {
-        clone: (o) => {
-            return _this.fns.clone(o);
-        },
-        extend: function() {
+        extend: function () {
             return _this.fns.extend.apply(this, arguments);
         },
-        xtoy: (x, y, e) => {
-            return _this.fns.xtoy(x, y, e);
+        clone: (o) => _this.fns.clone(o),        
+        xiny: (x, y, i) => _this.fns.xiny(x, y, i),
+        xtoy: (x, y, e) => _this.fns.xtoy(x, y, e)
+    };
+})();
+
+gumby.storage = (() => {
+    'use strict';
+    let _this = {
+        vars: {
+            cache: { }
+        },
+        fns: {
+            cache: {
+                add: (name, value) => {
+                    _this.vars.cache[name] = encodeURI(JSON.stringify(value));
+                },
+                get: (name) => {
+                    return JSON.parse(decodeURI(_this.vars.cache[name]));
+                },
+                remove: (name) => {
+                    delete _this.vars.cache[name];
+                },
+                clr: () => {
+                    _this.vars.cache = {};
+                }
+            },
+            cookies: {
+                add: (name, value, exDays, domain, path) => {
+                    let d = new Date();
+                    d.setTime(d.getTime() + ((exDays || 30) * 24 * 60 * 60 * 1000));
+                    let v = encodeURI(JSON.stringify(value || "")),
+                        e = d.toUTCString(),
+                        c = [
+                            name + "=" + v,
+                            "expires=" + e,
+                            "domain=" + (domain || window.location.host),
+                            "path=" + (path || "/")
+                        ].join(";");
+                    document.cookie = c;
+                },
+                get: (name) => {
+                    name += "=";
+                    let ca = document.cookie.split(';');
+                    for (let i = 0; i < ca.length; i++) {
+                        let c = ca[i];
+                        while (c.charAt(0) == ' ') c = c.substr(1);
+                        if (c.indexOf(name) == 0)
+                            return JSON.parse(decodeURI(c.substr(name.length, c.length)));
+                    }
+                    return "";
+                },
+                remove: (name) => {
+                    this.add(name, undefined, -1);
+                }
+            },
+            local: {
+                add: (name, value) => {
+                    _this.fns.local.remove(name);
+                    localStorage.setItem(name, encodeURI(JSON.stringify(value)));
+                },
+                get: (name) => {
+                    return JSON.parse(decodeURI(localStorage.getItem(name)));
+                },
+                remove: (name) => {
+                    localStorage.removeItem(name);
+                }
+            },
+            session: {
+                add: (name, value) => {
+                    _this.fns.session.remove(name);
+                    sessionStorage.setItem(name, encodeURI(JSON.stringify(value)));
+                },
+                get: (name) => {
+                    return JSON.parse(decodeURI(sessionStorage.getItem(name)));
+                },
+                remove: (name) => {
+                    sessionStorage.removeItem(name);
+                },
+                clr: () => {
+                    sessionStorage.clear();
+                }
+            }
         }
-    }
+    };
+    return {
+        cache: {
+            add: (name, value) => {
+                _this.fns.cache.add(name, value);
+            },
+            get: (name) => {
+                return _this.fns.cache.get(name);
+            },
+            remove: (name)=> {
+                _this.fns.cache.remove(name);
+            },
+            clr: ()=> {
+                _this.fns.cache.clr();
+            }
+        },
+        cookie: {
+            add: (name, value, exDays, domain, path) => {
+                _this.fns.cookies.add(name, value, exDays, domain, path);
+            },
+            get: (name) => {
+                return _this.fns.cookies.get(name);
+            },
+            remove: (name) => {
+                _this.fns.cookies.remove(name);
+            }
+        },
+        local: {
+            add: (name, value) => {
+                _this.fns.local.add(name, value);
+            },
+            get: (name) => {
+                return _this.fns.local.get(name);
+            },
+            remove: (name) => {
+                _this.fns.local.remove(name);
+            }
+        },
+        session: {
+            add: (name, value) => {
+                _this.fns.session.add(name, value);
+            },
+            get: (name) => {
+                return _this.fns.session.get(name);
+            },
+            remove: (name) => {
+                _this.fns.session.remove(name);
+            },
+            clr: () => {
+                _this.fns.session.clr();
+            }
+        }
+    };
 })();
 
 gumby.events = (() => {
+    'use strict';
     let _this = {
         vars: {
             subscribers : {}
@@ -125,7 +250,7 @@ gumby.events = (() => {
 })();
 
 gumby.data = (() => {
-    if(!gumby.object) return console.error("gumby.object is required for gumby.data");
+    'use strict';
     let _defaults = {
         sort: [],
         pager: { page: 0, size: 25 },
@@ -150,11 +275,11 @@ gumby.data = (() => {
         },
         verbose: false
     },
-    _options = gumby.object.extend({}, _defaults),
+    _options = gumby.extend({}, _defaults),
     _this = {
         fns: {
             bindOptions: function(opts){
-            	_options = gumby.object.extend({}, _defaults, _options, opts);
+            	_options = gumby.extend({}, _defaults, _options, opts);
             	for(var i = 0; i < _options.sort.length; i++)
             		if(typeof _options.sort[i].direction == "string"){
             			_options.sort[i].direction = _options.sort[i].direction.toLowerCase().indexOf("d")  > -1 ? -1 : 1;
@@ -162,37 +287,38 @@ gumby.data = (() => {
             	return _options;
             },
             load: (data) => {
-                var d = gumby.object.clone(data);
+                var d = gumby.clone(data);
                 d = _this.fns.filter(d, _options.filter);
                 d = _this.fns.sort(d, _options.sort);
                 d = _this.fns.page(d, _options.pager);
                 return d;
             },
-            filter: function(data, filter){			
-                if(data == undefined || filter == undefined || filter["filterOp"] == undefined) return data;
-                
-                let filterFn = function(f){
+            filter: function (data, filter) {                
+                if (data == undefined || filter == undefined || filter["filterOp"] == undefined) return data;
+                let filterFn = function (f) {
                     let q = [],
-                        ruleFn = function(r){
-                            with(r){
-                                return gumby.xiny(op, _options.operations.equations)
-                                    .replace("a", field)
-                                    .replace("b", value);
-                            }
+                        valFn = (v) => { return typeof v === "string" ? "'" + v + "'" : v; },
+                        ruleFn = function (r) {
+                            var e = gumby.xiny(r.op, _options.operations.equations, true);
+                            return "" +
+                                "((a, b) => { " +
+                                    "if(!a) return false; " +
+                                    "if(!b) b = null; " +
+                                    "return " + e + ";" +
+                                " })(gumby.xiny(" + valFn(r.field) + ", o), " + valFn(r.value) + ")";
                         };
-                        with(f){
-                            for(var i in rules){
-                                q.push(ruleFn(rules[i]));
-                            }
-                            for(var i in filters){
-                                q.push(filterFn(filters[i]));
-                            }
-                            return q.length > 0
-                                ? "(" + q.join(" " + gumby.xiny(filterOp, _options.operations.groupings) + " ") + ")"
-                                : "true";
-                        }
+
+                    for (var i in f.rules)
+                        q.push(ruleFn(f.rules[i]));
+                    
+                    for (var i in f.filters)
+                        q.push(filterFn(f.filters[i]));
+
+                    return q.length > 0
+                        ? "(" + q.join(" " + gumby.xiny(f.filterOp, _options.operations.groupings, true) + " ") + ")"
+                        : "true";
                 },
-                fn = new Function("o", "with(o){ return " + filterFn(filter) + "};");
+                fn = new Function("o", "return " + filterFn(filter) + ";");
                 _this.fns.verbose("gumby.data.filter", fn);
                 return data.filter(fn);
             },
@@ -220,24 +346,23 @@ gumby.data = (() => {
             },
             page: function(data, pager){
                 if(data === undefined || pager === undefined) return data;
-                with(pager){
-                    if(isNaN(page) || isNaN(size) || size === 0) return;
-                    let max = Math.floor(data.length / pager.size);				
-                    pager.page = pager.page < 0 ? 0 : pager.page;
-                    pager.page = pager.page > max ? max : pager.page;
-                    let 
-                        start = Math.max(page * size, 0),
-                        end = start + size;
-                        _this.fns.verbose("gumby.data.page", {
-                            start: start,
-                            end: end,
-                            min: 0,
-                            max: max,
-                            total: data.length
-                        });
+                if (isNaN(pager.page) || isNaN(pager.size) || pager.size === 0) return;
+
+                let max = Math.floor(data.length / pager.size);				
+                pager.page = pager.page < 0 ? 0 : pager.page;
+                pager.page = pager.page > max ? max : pager.page;
+                let 
+                    start = Math.max(pager.page * pager.size, 0),
+                    end = start + pager.size;
+                    _this.fns.verbose("gumby.data.page", {
+                        start: start,
+                        end: end,
+                        min: 0,
+                        max: max,
+                        total: data.length
+                    });
                     
-                    return data.slice(start, end);
-                }			
+                return data.slice(start, end);
             },
             verbose: (n, o) => {
                 if(!_options.verbose) return;
@@ -273,21 +398,21 @@ gumby.data = (() => {
 })();
 
 gumby.template = (() => {
-    if(!gumby.object) return console.error("gumby.object is required for gumby.template");
+    'use strict';
     var _defaults = {
         tagOpen		:	"{{",
         tagClose	:	"}}",
         tagArray	:	"[]",
         tagClosure	:	 "/"
     },
-    _options = gumby.object.extend({}, _defaults);
+    _options = gumby.extend({}, _defaults),
     _this = {        
         vars : {
             cache: {}
         },
         fns : {
             bindOptions: function(opts){
-            	_options = gumby.object.extend({}, _defaults, _options, opts);
+            	_options = gumby.extend({}, _defaults, _options, opts);
             	return _options;
             },
             tagFn: function(d, idx, ot, pt){
@@ -313,12 +438,12 @@ gumby.template = (() => {
                             x	=	f.indexOf(_options.tagOpen) > -1 && f.indexOf(_options.tagClose) > -1,
                             xf =	x ? _this.fns.fieldFn(f) : f;
                         if(!a && !c && xf.length > 0){
-                            d = d.substr(0, s) + (x ? '" + (function(){ var v = ""; with(p){ try{ v = eval(' + xf + '); } catch(e) { console.warn(e); } return v; }; })() + "' : '" + ' + xf + ' + "') + d.substr(e);
+                            d = d.substr(0, s) + (x ? '" + (function(){ var v = ""; debugger; with(p){ try{ v = eval(' + xf + '); } catch(e) { console.warn(e); } return v; }; })() + "' : '" + ' + xf + ' + "') + d.substr(e);
                         }
                         if(a)
                             if(!c){
                                 pt.unshift(f);
-                                d = d.substr(0, s) + '" + (function(o, p){ var t = ""; for(var i = 0; i < o.length; i++){ var itm = o[i]; with(itm){ t += "' + d.substr(e);
+                                d = d.substr(0, s) + '" + (function(o, p){ var t = ""; for(var i = 0; i < o.length; i++){ var itm = o[i]; debugger; with(itm){ t += "' + d.substr(e);
                             }else {
                                 pt.shift();
                                 d = d.substr(0, s) + '"; } } return t; })(' + f + (pt.length > 0 ? ', itm' : '') + ') + "' + d.substr(e);
@@ -347,7 +472,7 @@ gumby.template = (() => {
                 var d = template.slice().replace(/\r/g, "\\r").replace(/\n/g,"\\n");
                     
                 var f = _this.fns.tagFn(d);
-                var fn = new Function("r", "with(r){ return \"" + f + "\"; };");
+                var fn = new Function("r", "debugger; with(r){ return \"" + f + "\"; };");
                 _this.vars.cache[template] = fn;
                 return fn;
             },
@@ -378,4 +503,3 @@ gumby.template = (() => {
 		}    
     }
 })();
-
